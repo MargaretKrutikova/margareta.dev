37d02d7bd2314d6ad10a43b9f4b33126803f7edd
Margarita Krutikova <margaretkru@gmail.com>
2020-04-26 13:29:49 +0200

    Add sort import configuration, add button and index page

---
+++ src/components/button.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import styled from "styled-components"
+
+type Props = React.HTMLProps<HTMLButtonElement> & {
+  background?: string
+  fontSize?: string
+  fontWeight?: string
+  marginTop?: string
+  marginBottom?: string
+}
+
+export const Button = (props: Props) => (
+  <ButtonWrapper props={props}>{props.children}</ButtonWrapper>
+)
+
+const ButtonWrapper = styled.button<{ props: Props }>`
+  display: block;
+  border: none;
+  text-align: center;
+  box-sizing: border-box;
+  text-decoration: none;
+  padding: 10px 25px;
+  cursor: pointer;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+
+  background: ${props => props.props.background || "black"};
+  color: ${props => props.color || "rgb(255, 255, 255)"};
+  font-size: ${props => props.props.fontSize || "15px"};
+  font-weight: ${props => props.props.fontWeight || "600"};
+  border-radius: "6px",
+  margin-top: ${props => props.props.marginTop};
+  margin-bottom: ${props => props.props.marginBottom};
+
+  &:hover {
+    box-shadow: inset 0 0 100px 100px rgba(255, 255, 255, 0.25);
+  }
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,36 @@
-import { graphql, PageRendererProps, useStaticQuery } from "gatsby"
+import { Link, PageRendererProps } from "gatsby"
 import React from "react"
-import styled from "styled-components"
-import { Bio } from "../components/bio"
+
+import { Button } from "../components/button"
 import { Layout } from "../components/layout"
-import { FadeLink } from "../components/link"
 import { SEO } from "../components/seo"
-import { MarkdownRemark } from "../graphql-types"
-import { rhythm } from "../utils/typography"
-
-const StyledLink = styled(FadeLink)`
-  box-shadow: none;
-`
-
-const Title = styled.h3`
-  margin-bottom: ${rhythm(1 / 4)};
-`
 
 type Props = PageRendererProps
 
-const BlogIndex = (props: Props) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-        edges {
-          node {
-            excerpt
-            fields {
-              slug
-            }
-            frontmatter {
-              date(formatString: "MMMM DD, YYYY")
-              title
-              description
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+const Index = (props: Props) => {
+  const siteTitle = "Gatsby Starter Personal Website"
 
   return (
     <Layout location={props.location} title={siteTitle}>
-      <SEO
-        title="All posts"
-        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-      />
-      <Bio />
-      {posts.map(({ node }: { node: MarkdownRemark }) => {
-        const frontmatter = node!.frontmatter!
-        const fields = node!.fields!
-        const slug = fields.slug!
-        const excerpt = node!.excerpt!
-
-        const title = frontmatter.title || fields.slug
-        return (
-          <div key={slug}>
-            <Title>
-              <StyledLink to={slug}>{title}</StyledLink>
-            </Title>
-            <small>{frontmatter.date}</small>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: frontmatter.description || excerpt,
-              }}
-            />
-          </div>
-        )
-      })}
+      <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+      <h1>
+        Hey people{" "}
+        <span role="img" aria-label="wave emoji">
+          👋
+        </span>
+      </h1>
+      <p>Welcome to your new Gatsby website. You are on your home page.</p>
+      <p>
+        This starter comes out of the box with styled components and Gatsby's
+        default starter blog running on Netlify CMS.
+      </p>
+      <p>Now go build something great!</p>
+      <Link to="/blog/">
+        <Button marginTop="35px">Go to Blog</Button>
+      </Link>
     </Layout>
   )
 }
 
-export default BlogIndex
+export default Index
