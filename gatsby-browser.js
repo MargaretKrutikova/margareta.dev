5b1a63f13ffac783fdce07fb29ef008ffe2c97f0
Margarita Krutikova <margaretkru@gmail.com>
2020-05-11 22:13:39 +0200

    Experiment with code highlight color schema

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
-import "prism-themes/themes/prism-vsc-dark-plus.css"
 
+import "prism-themes/themes/prism-nord.css"
 import "./src/assets/global.css"
-import "./src/assets/prism-theme.css"
