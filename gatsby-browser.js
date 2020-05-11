95a8bba61b27611eb652e89ac25913641255e784
Margarita Krutikova <margaretkru@gmail.com>
2020-05-11 23:11:17 +0200

    Can't decide on code color schema

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,8 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "prism-themes/themes/prism-nord.css"
+// import "prism-themes/themes/prism-vsc-dark-plus.css"
+// import "prism-themes/themes/prism-nord.css"
+
 import "./src/assets/global.css"
+import "./src/assets/prism-theme.css"
