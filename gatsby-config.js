dba8b4a2886b8f1510fd278c376a5921d12fccac
Margarita Krutikova <MargaretKru@gmail.com>
2020-06-11 13:21:25 +0200

    Remove offline plugin to fix blank page issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
 
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
