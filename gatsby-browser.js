38162ef19c58499debc88eef361a110aa6dab17f
Margarita Krutikova <margaretkru@gmail.com>
2020-06-23 22:33:09 +0200

    Small typography changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-
 // import "prism-themes/themes/prism-vsc-dark-plus.css"
 // import "prism-themes/themes/prism-nord.css"
 
